1b34eba1bc3b7778b03eb1c83ccb79a993d7da5a
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2021-12-08 17:22:46 +0000

    Create oc_deploy.bash

---
+++ s/oc_deploy.bash
@@ -0,0 +1,10 @@
+#!/user/bin/env bash
+
+sudo yum install -y wget
+wget https://raw.githubusercontent.com/logrhythm/versions/master/lrctl
+chmod +x lrctl
+# For CentOS 8:
+# sudo yum erase podman buildah
+./lrctl init
+./lrctl metrics start
+./lrctl open-collector start
